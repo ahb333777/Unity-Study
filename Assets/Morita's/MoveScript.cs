2999a0ccc582177e31a78886d53dfb821eb8f28a
unknown <nwuser@GM2150161.da.iwasaki.ac.jp>
2016-03-07 14:55:02 +0900

    コミット制作(森田)

---
+++ Assets/Morita's/MoveScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			GameObject.transform.position += new Vector3(1,0,0);
+		}
+	}
+}
